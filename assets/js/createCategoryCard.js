99863c6a2eaa3794cdfbb5fff3e7a18d38bbae85
lucaso-silva <luc.osilva@gmail.com>
2023-09-30 18:56:17 -0700

    fixed: function create card category bg image and bg color

--- assets/js/createCategoryCard.js
+++ assets/js/createCategoryCard.js
@@ -1,15 +1,22 @@
 const containerCards = document.querySelector(".container-cards")
 
 export default function createCategoryCard(data) {
+    //console.log(data);
+    let backgroundImg = data.backgroundImg;
+    let backgroundColor = data.color;
     let category =  data.title;
+    let id = data.id;
     let currentDailyTimeInfo = data.timeframes.daily.current;
     let currentWeeklyTimeInfo = data.timeframes.weekly.current;
     let currentMonthTimeInfo = data.timeframes.monthly.current
     
-    console.log("category: " + category + ". daily: " + currentDailyTimeInfo + " . weekly: " + currentWeeklyTimeInfo + " . monthly: " + currentMonthTimeInfo);
+    //console.log("category: " + category + ". daily: " + currentDailyTimeInfo + " . weekly: " + currentWeeklyTimeInfo + " . monthly: " + currentMonthTimeInfo);
     
     let divCategoryCard = document.createElement("div");
     divCategoryCard.classList.add("card-category");
+    divCategoryCard.setAttribute("id", `card-${id}`);
+    divCategoryCard.style.backgroundImage = `url('${backgroundImg}')`;
+    divCategoryCard.style.backgroundColor = backgroundColor;
 
     let divWrapper = document.createElement("div");
     let divCurrentInfo = document.createElement("div");
